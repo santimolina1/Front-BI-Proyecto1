c52cd726033714a022d0d28d3f7fe06aef126bc2
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-10-12 11:00:12 -0500

    xd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Prediction from './components/Prediction';
 import Upload from './components/Upload';
-import './App.css'; // Importamos el CSS para estilos de la barra de navegación
+import './App.css';
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Cambiar el título basado en la ruta
+    switch (location.pathname) {
+      case '/prediction':
+        document.title = "Predicción - ODS Web";
+        break;
+      case '/upload':
+        document.title = "Subir Archivo - ODS Web";
+        break;
+      default:
+        document.title = "Inicio - ODS Web";
+        break;
+    }
+  }, [location.pathname]);
+
   return (
-    <Router>
+    <div>
       <header className="navbar">
         <div className="navbar-container">
           <h1 className="navbar-logo">ODS Web</h1>
@@ -28,8 +45,14 @@ const App = () => {
           <Route path="/upload" element={<Upload />} />
         </Routes>
       </div>
-    </Router>
+    </div>
   );
 };
 
-export default App;
+const WrappedApp = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default WrappedApp;
